6b56f390707356a8b6f1acddcf867ceb2b6b0b88
DavideB45 <dadewww@gmail.com>
2023-11-29 15:45:24 +0100

    simple demo

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from utils.map import Map
 
 level = Map()
 
-level.apply_action('S')
-level.apply_action('THROW','carrot', 'S')
-level.render()
+
+for i in range(7):
+    level.go_near_pony(show_steps=True)
+    throw_direction = level.throw_to_pony_direction()
+    level.apply_action('THROW', what='carrot', where=throw_direction)
+    level.render(delay=3)
+
+#print(level.get_agent_position())
+#print(level.get_pony_position())
+#level.print_every_position()
