f010ad9fa1eec9a1ffb28479744fa0092b0e0cce
DavideB45 <dadewww@gmail.com>
2023-11-29 16:56:15 +0100

    Extended main
    In theory it should work but when applying saddle it throws an error

--- main.py
+++ main.py
@@ -2,17 +2,31 @@ from utils.map import Map
 
 level = Map()
 
+#go to saddle
+level.go_to_element(element='saddle',show_steps=True, maxDistance=0, minDistance=0)
+level.render()
 
+#pick up saddle
+level.apply_action('PICKUP')
+level.render()
+
+#go to pony and throw carrots
 for i in range(3):
-    level.go_near_pony(show_steps=True)
-    throw_direction = level.throw_to_pony_direction()
+    level.go_to_element(element='pony',show_steps=True)
+    throw_direction = level.get_pony_direction()
     level.apply_action('THROW', what='carrot', where=throw_direction)
     level.render(delay=3)
 
-level.go_near_pony(show_steps=True, maxDistance=1)
-throw_direction = level.throw_to_pony_direction()
+#saddle pony
+level.go_to_element(element='pony',show_steps=True, maxDistance=1)
+saddle_direction = level.get_pony_direction()
+level.apply_action('APPLY', what='saddle', where=saddle_direction)
+level.render()
+
 #ride pony
-level.apply_action('RIDE', where=throw_direction)
+level.go_to_element(element='pony',show_steps=True, maxDistance=1)
+ride_direction = level.get_pony_direction()
+level.apply_action('RIDE', where=ride_direction)
 level.render()
 #print(level.get_agent_position())
 #print(level.get_pony_position())
