2a009312bfc2060f5b99d86aefa72dde4fb0f9a3
Chomp97 <yuri.negri2ds@gmail.com>
2023-11-29 17:36:16 +0100

    directionality and prompts for kb

--- KBS/kb.pl
+++ KBS/kb.pl
@@ -1,7 +1,115 @@
 :- dynamic position/4.
 :- dynamic carrots.
+:- dynamic stepping_on/3.
+
+% To translate into Prolog:
+% Chance of succeeding a mounting action is: 5 * (exp level + steed tameness)
+% cannot attempt to mount a steed if any of the following conditions apply:
+% 
+%     You are already riding.
+%     You are hallucinating.
+%     Your have wounded legs.
+%     Your encumbrance is stressed or worse.
+%     You are blind and not telepathic.
+%     You are punished.
+%     You or your steed are trapped.
+%     You are levitating and cannot come down at will.
+%     You are wearing rusty or corroded body armor.
+% 
+% You will always fail and slip if any of the following apply:[3]
+% 
+%     You are confused.
+%     You are fumbling.
+%     You have slippery fingers.
+%     Your steed's saddle is cursed.
 
 0 :- carrots.
 
 action(throw) :- carrots > 0.
 %TODO: it is currently unused
+
+% We will need to eventually pick the carrot
+action(pick) :- 
+    stepping_on(agent,ObjClass,_),
+    is_pickable(ObjClass). 
+
+% We need to check this if we are to throw carrots at a horse.
+is_aligned(R1,C1,R2,C2) :- R1 == R2; C1 == C2; ((R1 is R2+X;R1 is R2-X), (C1 is C2+X;C1 is C2-X)).
+
+% Directionality and space conditions, taken from handson2
+% test the different condition for closeness
+% two objects are close if they are at 1 cell distance, including diagonals
+is_close(R1,C1,R2,C2) :- R1 == R2, (C1 is C2+1; C1 is C2-1).
+is_close(R1,C1,R2,C2) :- C1 == C2, (R1 is R2+1; R1 is R2-1).
+is_close(R1,C1,R2,C2) :- (R1 is R2+1; R1 is R2-1), (C1 is C2+1; C1 is C2-1).
+
+% compute the direction given the starting point and the target position
+% check if the direction leads to a safe position
+% D = temporary direction - may be unsafe
+%Direction = the definitive direction 
+next_step(R1,C1,R2,C2, D) :-
+    ( R1 == R2 -> ( C1 > C2 -> D = west; D = east );
+    ( C1 == C2 -> ( R1 > R2 -> D = north; D = south);
+    ( R1 > R2 ->
+        ( C1 > C2 -> D = northwest; D = northeast );
+        ( C1 > C2 -> D = southwest; D = southeast )
+    ))).
+    % safe_direction(R1, C1, D,Direction).
+
+% check if the selected direction is safe
+safe_direction(R, C, D,Direction) :- resulting_position(R, C, NewR, NewC, D),
+                                      ( safe_position(NewR, NewC) ->Direction = D;
+                                      % else, get a new close direction
+                                      % and check its safety
+                                      close_direction(D, ND), safe_direction(R, C, ND,Direction)
+                                      ).
+
+% a square if unsafe if there is a trap or an enemy
+unsafe_position(R, C) :- position(trap,_, R, C).
+unsafe_position(R, C) :- position(enemy,_, R, C).
+unsafe_position(R,C) :- 
+    position(enemy,_, ER, EC), 
+    is_close(ER, EC, R, C).
+
+%%%% known facts %%%%
+opposite(north, south).
+opposite(south, north).
+opposite(east, west).
+opposite(west, east).
+opposite(northeast, southwest).
+opposite(southwest, northeast).
+opposite(northwest, southeast).
+opposite(southeast, northwest).
+
+resulting_position(R, C, NewR, NewC, north) :-
+    NewR is R-1, NewC = C.
+resulting_position(R, C, NewR, NewC, south) :-
+    NewR is R+1, NewC = C.
+resulting_position(R, C, NewR, NewC, west) :-
+    NewR = R, NewC is C-1.
+resulting_position(R, C, NewR, NewC, east) :-
+    NewR = R, NewC is C+1.
+resulting_position(R, C, NewR, NewC, northeast) :-
+    NewR is R-1, NewC is C+1.
+resulting_position(R, C, NewR, NewC, northwest) :-
+    NewR is R-1, NewC is C-1.
+resulting_position(R, C, NewR, NewC, southeast) :-
+    NewR is R+1, NewC is C+1.
+resulting_position(R, C, NewR, NewC, southwest) :-
+    NewR is R+1, NewC is C-1.
+
+close_direction(north, northeast).
+close_direction(northeast, east).
+close_direction(east, southeast).
+close_direction(southeast, south).
+close_direction(south, southwest).
+close_direction(southwest, west).
+close_direction(west, northwest).
+close_direction(northwest, north).
+
+unsafe_position(_,_) :- fail.
+% \+ means "the proposition is not entailed by KB". Sort of a not, but more general
+safe_position(R,C) :- \+ unsafe_position(R,C).
+
+% we need to pick a carrot if we are stepping on it. 
+is_pickable(carrot).
