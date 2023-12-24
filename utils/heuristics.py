f8bcd6f4ec009b5bc5764ebcffbf265b5775bc07
Chomp97 <yuri.negri2ds@gmail.com>
2023-12-24 23:33:59 +0100

    removed unnecessary function

--- utils/heuristics.py
+++ utils/heuristics.py
@@ -1,18 +1,10 @@
-# import math
-
-
-#def euclidean_distance(positions:list(tuple), toCompare: int) -> tuple:
-#    return min(positions, key=lambda x: math.sqrt(sum((x[i]-toCompare[i])**2 for i in range(len(x)))))
-
+import numpy as np
+from typing import List, Tuple
 
 # I am lazy and I use numpy :D 
 # Jokes aside, doing all the involved computations by hand would just be 
 # tedious. There are more flashy-pythonic ways of doing it, 
 # but in this way is more elegant 
-import numpy as np
-from typing import List, Tuple
-
-
 def euclidean_distance(positions:List[Tuple], toCompare:Tuple[int,int]) -> Tuple[int,int]:
     '''Compute the closest point to toCompare among those in the positions list, 
     according to the euclidean distance.
