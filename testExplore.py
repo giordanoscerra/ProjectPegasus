14ac67f15bb9dc7767ec95384fb85211dd60649c
DavideB45 <dadewww@gmail.com>
2023-12-26 14:55:22 +0100

    renaming

---
+++ testExplore.py
@@ -0,0 +1,25 @@
+from utils.map import Map
+from utils.agent import Agent
+
+lvl = Map(pony=False, level=1)
+#this is a knight whose name is samurai
+samurai = Agent()
+
+for i in range(120):
+    to_explore = samurai.explore(lvl)
+    if to_explore == '':
+        break
+    lvl.apply_action(actionName=to_explore)
+    lvl.render()
+print("lvl explore 1: Done")
+
+lvl = Map(pony=False, level=2)
+#this is a knight whose name is samurai
+samurai = Agent()
+
+for i in range(120):
+    to_explore = samurai.explore(lvl)
+    if to_explore == '':
+        break
+    lvl.apply_action(actionName=to_explore)
+print("lvl explore 2: Done")
