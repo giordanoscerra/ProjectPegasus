4563fa288a5f61a9932c3af51e6dedf4cb6f136d
Davide <davidemarchi00@gmail.com>
2023-12-28 18:01:14 +0100

    Starting to pacify

---
+++ testPacifySteed.py
@@ -0,0 +1,37 @@
+from utils.map import Map
+from utils.agent import Agent
+import math
+from utils.heuristics import manhattan_distance, infinity_distance, euclidean_distance
+
+def calculate_throw_range(strength):
+    return 0 + math.floor(strength/2)
+
+for i in range(3, 26):
+    print(i, ' range ', calculate_throw_range(i))
+
+level = Map(pony=False)
+knight = Agent()
+
+knight.percept(level)
+level.render()
+
+
+
+knight.get_carrot(level = level, heuristic= lambda x,y: manhattan_distance([x],y)[1])
+#level.render()
+
+# https://nethackwiki.com/wiki/Throw#Food
+agent_strenght = level.get_agent_strength() 
+max_throw_range = 6
+
+knight.go_to_closer_element(level,element='pony',show_steps=True, delay=0.2, heuristic= lambda x,y: manhattan_distance([x],y)[1], maxDistance=max_throw_range)
+#level.render()
+
+#level.align_with_pony()
+#level.render()
+
+#pony_direction = level.get_pony_direction()
+#level.render()
+
+level.apply_action(actionName='THROW', what='carrot', where='S')
+level.render()
