65978b0e126175f4ea25fdcd5f977302490273a7
DavideB45 <dadewww@gmail.com>
2023-12-29 23:19:18 +0100

    Moved some tests

--- test_folder/testProcessInventory.py
+++ test_folder/testProcessInventory.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.join(sys.path[0], '..'))
 from utils.agent import Agent
 from utils.heuristics import manhattan_distance
 from utils.map import Map
--- test_folder/testSearch.py
+++ test_folder/testSearch.py
@@ -1,6 +1,11 @@
+import sys
+import os
+sys.path.append(os.path.join(sys.path[0], '..'))
 from utils.map import Map
 from utils.agent import Agent
 
+#this test can't work if you don't change the explore_subtask method in agent.py
+#please don't
 
 lvl = Map(pony=False, level=3)
 lvl.render()
