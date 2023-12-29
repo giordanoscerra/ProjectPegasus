c005edf3d2ba9f6dca5941409935a1af1229eec1
DavideB45 <dadewww@gmail.com>
2023-12-29 17:15:11 +0100

    very easy test

--- testGoToCloserElement.py
+++ testGoToCloserElement.py
@@ -1,11 +1,15 @@
 from utils.map import Map
 from utils.agent import Agent
+from utils.general import decode
 
 lvl = Map(pony=False, level=6)
 #the name of the knight is Yoritomo
 yoritomo = Agent()
 
-lvl.render()
+yoritomo.go_to_closer_element(lvl,element='saddle',show_steps=False,delay=0.2)
 
-yoritomo.go_to_closer_element(lvl,element='saddle',show_steps=True,delay=0.2)
-lvl.render()
+message=decode(lvl.state['message'])
+if 'You see here a saddle' in message:
+    print('test go to closer element passed')
+else:
+    print('test go to closer element failed')
