bbe1143a5bfb551e306e5c7df6a0f8f798ca4b79
DavideB45 <dadewww@gmail.com>
2024-01-01 22:33:47 +0100

    moved test.act

--- test_folder/testAct.py
+++ test_folder/testAct.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.join(sys.path[0], '..'))
 from utils.heuristics import manhattan_distance, infinity_distance, euclidean_distance
 from utils.map import Map
 from utils.agent import Agent
