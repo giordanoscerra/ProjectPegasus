561b76849b7c7b5fd51b337969788d9024bfca49
Chomp97 <yuri.negri2ds@gmail.com>
2024-01-02 15:12:44 +0100

    removed wrong procedure from testact

--- test_folder/testAct.py
+++ test_folder/testAct.py
@@ -9,21 +9,6 @@ from utils import exceptions
 # spawn level
 level = Map(pony=True, level=74)
 agent = Agent()
-# agent.percept(level)
-# print(f'pony hostile? : {agent.kb.query_hostile()}')
-# print(f'pony position: {agent.kb.get_element_position_query("pony")}')
-# print(f'carrot position: {agent.kb.get_element_position_query("carrot")}')
-# carrots = agent.kb.query_quantity("carrot")
-# print(f'how many carrots? : {agent.kb.query_quantity("carrot")}')
-# agent.kb.update_quantity("carrot", 0)
-# 
-# agent.act(level)
-# # let him ACT !!!!!!!!!
-# while level.rewards[-1] != 100:
-#     level.render()
-#     agent.percept(level)
-#     agent.kb.update_quantity("carrot", agent.kb.query_quantity("carrot")-carrots)
-#     agent.act(level)
 # this is important af
 agent.percept(level)
 level.render()
