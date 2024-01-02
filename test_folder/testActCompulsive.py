dfd20d21b95ef2f4853933fa4a0b647aef8dc922
giordanoscerra <121350206+giordanoscerra@users.noreply.github.com>
2024-01-02 20:57:00 +0100

    compulsive testAct

---
+++ test_folder/testActCompulsive.py
@@ -0,0 +1,18 @@
+import sys
+import os
+sys.path.append(os.path.join(sys.path[0], '..'))
+from utils.heuristics import manhattan_distance, infinity_distance, euclidean_distance
+from utils.map import Map
+from utils.agent import Agent
+from utils import exceptions
+
+while True:
+    # spawn level
+    level = Map(pony=True, level=-1)
+    agent = Agent()
+    # this is important af
+    agent.percept(level)
+    level.render()
+    # let him ACT !!!!!!!!!
+    while(level.get_agent_position() != level.get_pony_position()):
+        agent.act(level, show_steps=True, graphic=False, delay=0)
