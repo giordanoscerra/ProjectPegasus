5e5f83a1c4c225d29e0a8ad7d7ef47bf414dda97
DavideB45 <dadewww@gmail.com>
2024-01-03 11:40:55 +0100

    small fix in KB

--- test_folder/testAct.py
+++ test_folder/testAct.py
@@ -7,15 +7,14 @@ from utils.agent import Agent
 from utils import exceptions
 
 # spawn level
-level = Map(pony=True, level=1)
+level = Map(pony=True, level=-1)
 agent = Agent()
 # this is important af
 agent.percept(level)
 level.render()
-agent.act(level)
 # let him ACT !!!!!!!!!
 while(not level.is_episode_over()):
-    agent.act(level)
+    agent.act(level, show_steps=True, graphic=False, delay=0.0)
 
 print(level.rewards)
 exit(agent.actions_performed)
