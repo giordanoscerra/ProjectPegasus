aabc19809af88768abee7068849c381521c8bda2
M4rinz <134485532+M4rinz@users.noreply.github.com>
2024-01-03 10:35:19 +0100

    fixed assert_has with 'saddled pony'

--- utils/agent.py
+++ utils/agent.py
@@ -127,9 +127,13 @@ class Agent():
                 if 'picks up a '+x in msg:
                     # 4: all those messages (hopefully!) start with 'The '
                     picker = msg[4:msg.find(' picks up a '+x)]
+                    if('pony' in picker):
+                        picker='pony'
                     self.kb.assert_has(owner=picker,item=x)
                 if 'drops a '+x in msg:
                     dropper = msg[4:msg.find(' drops a '+x)]
+                    if('pony' in dropper):
+                        dropper = 'pony'
                     self.kb.retract_has(owner=dropper,item=x)
                 for steed in self.kb._categories['steed']:
                     for synonimous in ['eats','devours']:
