115e261438e3692720d27656459a43533e9f2839
giordanoscerra <121350206+giordanoscerra@users.noreply.github.com>
2024-01-04 10:02:15 +0100

    fix slow render

--- utils/map.py
+++ utils/map.py
@@ -75,6 +75,7 @@ class Map:
             display.display(plt.gcf())
             print(bytes(self.state['message']).decode('utf-8').rstrip('\x00'))
             display.clear_output(wait=True)
+            plt.close()
     
     # This function is used only by the testagent.py script
     def get_element_position(self, element:str) -> List[Tuple[int,int]]:
