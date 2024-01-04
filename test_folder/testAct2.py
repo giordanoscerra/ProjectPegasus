1329082e1e4580fcc0a5a92651c267d1e7efa349
DavideB45 <dadewww@gmail.com>
2024-01-04 10:52:25 +0100

    test act gracefully interrupted

--- test_folder/testAct2.py
+++ test_folder/testAct2.py
@@ -3,12 +3,12 @@ import os
 sys.path.append(os.path.join(sys.path[0], '..'))
 from utils.heuristics import manhattan_distance, infinity_distance, euclidean_distance
 from utils.map import Map
-from utils.agent import Agent
+from utils.agent2 import Agent
 from utils import exceptions
 import signal
 
 def handler(signum, frame):
-    stats = open("stats.txt", "a")
+    stats = open("stats2.txt", "a")
     stats.write(f'rewards:<{str(-1)}> steps:<{str(-1)}>\n')
     stats.close()
     sys.exit(0)
@@ -25,7 +25,7 @@ while(not level.is_episode_over()):
     agent.act(level, show_steps=False, graphic=False, delay=0.0)
 
 #save stats on a file called stats.txt
-stats = open("stats.txt", "a")
+stats = open("stats2.txt", "a")
 stats.write(f'rewards:<{str(level.rewards[-1])}> steps:<{str(len(level.rewards))}>\n')
 stats.close()
 
